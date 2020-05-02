25e8d74c02e39d82db2705460290b1d9ce7b667e
Alex Ovchinnikov <SanSeyp6@gmail.com>
2020-05-02 21:35:12 +0300

    Implemented displaying TODO details

--- src/main/java/ru/hse/todo/console/Main.java
+++ src/main/java/ru/hse/todo/console/Main.java
@@ -1,5 +1,8 @@
 package ru.hse.todo.console;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
@@ -10,6 +13,7 @@ import ru.hse.todo.Todo;
 import ru.hse.todo.TodoList;
 import ru.hse.todo.console.options.ChainedOption;
 import ru.hse.todo.console.options.ConsoleOption;
+import ru.hse.todo.console.options.DisplayTodoDetails;
 import ru.hse.todo.console.options.DisplayTodosOption;
 import ru.hse.todo.console.options.NotImplementedOption;
 import ru.hse.todo.console.options.Option;
@@ -24,39 +28,64 @@ public final class Main
     public static void main( String[] args )
     {
     	final List<Todo> list = new LinkedList<Todo>();
-    	list.add(new SimpleTodo());
-    	list.add(new SimpleTodo());
-    	list.add(new SimpleTodo());
+    	final DateTimeFormatter yyyy_MM_dd_HH_mm = DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm");
+    	list.add(
+    		new SimpleTodo(
+    			"Task 1",
+    			"Description 1",
+    			LocalDateTime.parse(
+    				"2020.05.03 14:00",
+    				yyyy_MM_dd_HH_mm
+    			).atZone(ZoneId.systemDefault())
+    		)
+    	);
+    	list.add(
+        	new SimpleTodo(
+        		"Task 2",
+        		"Description 2",
+        		LocalDateTime.parse(
+        			"2020.05.04 15:00",
+        			yyyy_MM_dd_HH_mm
+        		).atZone(ZoneId.systemDefault())
+        	)
+        );
+    	list.add(
+           	new SimpleTodo(
+           		"Task 3",
+           		"Description 3",
+           		LocalDateTime.parse(
+           			"2020.05.06 15:00",
+           			yyyy_MM_dd_HH_mm
+           		).atZone(ZoneId.systemDefault())
+           	)
+        );   	
     	final TodoList todos = new SimpleTodoList(list);
-    	final Option option = new NotImplementedOption();
+    	final Option notImplemented = new NotImplementedOption();
+    	final Scanner scanner = new Scanner(System.in);
         new ConsoleApp(
         	new MainMenu(),
         	new ConsoleOption(
-        		new Scanner(System.in),
+        		scanner,
         		new ChainedOption(
         			1,
         			new DisplayTodosOption(todos),
-        			new ChainedOption(
-        				2, 
-        				option,
-        				new ChainedOption(
-        					3,
-        					option,
-        					new ChainedOption(
-        						4,
-        						option,
-        						new ChainedOption(
-        							5,
-        							option,
-        							new ChainedOption(
-        								6,
-        								new QuitOption(),
-        								option
-        							)
-        						)
-        					)
-        				)
-        			)
+       				new ChainedOption(
+       					2,
+       					new DisplayTodoDetails(list, new ConsoleSelection(todos, scanner)),
+       					new ChainedOption(
+       						3,
+       						notImplemented,
+       						new ChainedOption(
+       							4,
+       							notImplemented,
+       							new ChainedOption(
+       								5,
+       								new QuitOption(),
+       								notImplemented
+       							)
+       						)
+       					)
+       				)
         		)
         	)
         ).run();
--- src/main/java/ru/hse/todo/console/MainMenu.java
+++ src/main/java/ru/hse/todo/console/MainMenu.java
@@ -4,11 +4,10 @@ public final class MainMenu {
 	void print() {
 		System.out.println("-------------- Console TODOs ---------------");
 		System.out.println("1. Display TODOs");
-		System.out.println("2. Select TODO");
-		System.out.println("3. Display selected TODO details");
-		System.out.println("4. Add new TODO");
-		System.out.println("5. Remove selected TODO");
-		System.out.println("6. Quit");
+		System.out.println("2. Display TODO details");
+		System.out.println("3. Add new TODO");
+		System.out.println("4. Remove TODO");
+		System.out.println("5. Quit");
 		System.out.print("Type option number and press \'Enter\' key: ");
 	}
 }
