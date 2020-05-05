08710bbf4a6929a1572df328511b48e661ed9319
Alex Ovchinnikov <SanSeyp6@gmail.com>
2020-05-05 03:45:58 +0300

    Added info to README.md

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,4 +2,7 @@ module ru.hse.todo.console {
 	exports ru.hse.todo;
 	exports ru.hse.todo.console;
 	exports ru.hse.todo.console.options;
+	exports ru.hse.todo.outputs;
+	exports ru.hse.todo.storages;
+	exports ru.hse.todo.todos;
 }
